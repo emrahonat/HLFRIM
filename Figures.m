5c07080e83357dfaaa9b2b8bb07031ea037d7c48
emrah onat <eonat87@yahoo.com>
2025-02-27 20:27:21 +0100

    first commit

---
+++ Figures.m
@@ -0,0 +1,9 @@
+%% Please cite these 2 papers
+% 
+% [1] Onat, E., Özkazanç, Y. (2025). Hybrid-Level Fusion of Radar Imaging Methods. International Journal of Engineering and Geosciences, Volume (Issue), page numbers.
+% [2] E. Onat and Y. Özkazanç, "An analysis of Doppler beam sharpening technique used in fighter aircraft," 2018 26th Signal Processing and Communications Applications Conference (SIU), Izmir, Turkey, 2018, pp. 1-4, doi: 10.1109/SIU.2018.8404521.
+% 
+% Dr. Emrah Onat - TOBB University of Economics and Technology, Ankara, Turkey (eonat87@yahoo.com)
+% Dr. Yakup Ozkazanc - Hacettepe University, Ankara, Turkey
+%
+% Figure 12, Figure 13, Figure 14, and Figure 15 of Hybrid-Level Fusion of Radar Imaging Methods
